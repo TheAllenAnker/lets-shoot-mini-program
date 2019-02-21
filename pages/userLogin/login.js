f4967f31e94c1b09f04694c2e1a3fc670d3aefde
TheAllenAnker <theallenanker@gmail.com>
2019-02-21 21:15:04 +0800

    Add login and register pages.

---
+++ pages/userLogin/login.js
@@ -0,0 +1,61 @@
+// pages/userLogin/login.js
+const app = getApp()
+
+Page({
+  data: {
+
+  },
+
+  doLogin: function(e) {
+    var formObj = e.detail.value;
+    var username = formObj.username;
+    var password = formObj.password;
+
+    if (username.length == 0 || password.length == 0) {
+      wx.showToast({
+        title: '用户名或密码不能为空',
+        icon: 'none',
+        duration: 3000
+      })
+    } else {
+      wx.showLoading({
+        title: '请等待...',
+      });
+      wx.request({
+        url: app.serverUrl + '/login',
+        method: 'POST',
+        data: {
+          username: username,
+          password: password
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function (res) {
+          wx.hideLoading();
+          var status = res.data.status;
+          if (status == 200) {
+            wx.showToast({
+              title: '登录成功',
+              icon: 'success',
+              duration: 3000
+            })
+            app.userInfo = res.data.data;
+          } else {
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'none',
+              duration: 3000
+            })
+          }
+        }
+      })
+    }
+  },
+
+  goRegisterPage: function() {
+    wx.navigateTo({
+      url: '../userRegist/regist',
+    })
+  }
+})
--- pages/userRegist/regist.js
+++ pages/userRegist/regist.js
@@ -4,4 +4,57 @@ Page({
     data: {
 
     },
+
+    doRegist: function(e) {
+      var formObj = e.detail.value;
+      var username = formObj.username;
+      var password = formObj.password;
+      
+      if (username.length == 0 || password.length == 0) {
+        wx.showToast({
+          title: '用户名或密码不能为空',
+          icon: 'none',
+          duration: 3000
+        })
+      } else {
+        wx.showLoading({
+          title: '请等待...',
+        });
+        wx.request({
+          url: app.serverUrl + '/register',
+          method: 'POST',
+          data: {
+            username: username,
+            password: password
+          },
+          header: {
+            'content-type': 'application/json'
+          },
+          success: function(res) {
+            wx.hideLoading();
+            var status = res.data.status;
+            if (status == 200) {
+              wx.showToast({
+                title: '注册成功',
+                icon: 'success',
+                duration: 3000
+              })
+              app.userInfo = res.data.data;
+            } else {
+              wx.showToast({
+                title: res.data.msg,
+                icon: 'none',
+                duration: 3000
+              })
+            }
+          }
+        })
+      }
+    },
+
+    goLoginPage: function() {
+      wx.redirectTo({
+        url: '../userLogin/login',
+      })
+    }
 })
